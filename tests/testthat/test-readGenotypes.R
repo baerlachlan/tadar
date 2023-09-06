bee96ff0176e8c519c91cede500e6b425b3f1b10
Lachlan Baer <baerlachlan@gmail.com>
2023-09-06 16:54:18 +0930

    Make sample names in example VCF more logical

--- tests/testthat/test-readGenotypes.R
+++ tests/testthat/test-readGenotypes.R
@@ -1,8 +1,7 @@
 fl <- system.file("extdata", "chr1.vcf.bgz", package="darr")
 
 test_that("readGenotypes returns the expected names", {
-    mcolNames <- c("S2", "S7", "S9", "S10", "S19", "S20",
-      "S3", "S6", "S11", "S12", "S15", "S16", "S18")
+    mcolNames <- paste0("sample", 1:13)
     geno_fl <- readGenotypes(fl)
     expect_equal(names(geno_fl), NULL)
     expect_equal(names(mcols(geno_fl)), mcolNames)
