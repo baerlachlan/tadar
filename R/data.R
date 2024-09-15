a71ae7aa1d686817982bd433b586bc0a63c95690
baerlachlan <baerlachlan@gmail.com>
2024-09-15 21:18:14 +0930

    Add `modP()`

--- R/data.R
+++ R/data.R
@@ -14,3 +14,15 @@
 #' @rdname chr1_genes
 #' @usage data(chr1_genes)
 "chr1_genes"
+
+#' @title Differential expression example data
+#'
+#' @description *p*-values from differential expressiong testing for
+#'  example usage.
+#'
+#' @return A 716 x 5 tibble object.
+#'
+#' @name chr1_tt
+#' @rdname chr1_tt
+#' @usage data(chr1_tt)
+"chr1_tt"
